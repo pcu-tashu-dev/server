329c476c8f4109d54d3c1c9401ab7332443eb670
rlaxogh5079 <rlaxogh507906@gmail.com>
2025-09-18 00:32:21 +0900

    :bug: 데이터베이스 초기화 로직 변경 누락 수정

--- database/init_db.py
+++ database/init_db.py
@@ -1,10 +1,11 @@
-from database.connection import DBObject
+from database.connection import DBFactory
 from model.base_class import Base
+from model.station import Station
 import asyncio
 
 async def create_all():
-    await DBObject.init_async_db()
-    async with DBObject.engine.begin() as conn:
+    postgres_connector = await DBFactory.create(driver="postgres")
+    async with postgres_connector.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 if __name__ == "__main__":
