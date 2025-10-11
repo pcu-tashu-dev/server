9016c15bfa0108d84d389c229add8b776992ee1d
최다연 <choedayeon@choedayeon-ui-MacBookAir.local>
2025-10-11 18:09:34 +0900

    :sparkles: 유저 정보 중복 검사 기능 추가

--- api/users/check_duplicate.pb.js
+++ api/users/check_duplicate.pb.js
@@ -1,15 +1,35 @@
 routerAdd("POST", "/users/duplicate/{param}", (e) => {
   const { ok, fail } = require(`${__hooks}/lib/response.js`);
-  const { parse } = require(`${__hooks}/lib/body.js`);
   try {
     const param = e.request.pathValue("param");
+    const query = e.request.url.query().get("data");
 
     if (param != "username" && param != "email") {
       return fail(e, "param must be 'username' or 'email'", 400);
     }
 
-    const body = parse(e, { data: "" }); // FIXME: body 데이터 -> Query Parameter로 변경 필요
-    const data = body.data;
+    if (!query) {
+      return fail(
+        e,
+        `${param}'s value not founded. Usage: ${param}?data=your-data`,
+        400
+      );
+    }
+
+    let user;
+    try {
+      user = $app.findFirstRecordByData("users", param, query);
+    } catch (error) {
+      if (!(error instanceof GoError)) {
+        return fail(e, error.message);
+      }
+    }
+
+    if (user) {
+      return fail(e, `'${query}' '${param}' already taken`, 409);
+    } else {
+      return ok(e, { message: `'${query}' '${param}' data can be used` }, 200);
+    }
   } catch (err) {
     return fail(e, err.message);
   }
